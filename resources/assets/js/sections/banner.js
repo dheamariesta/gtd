37442b099ae5e77e80f9f3a014ec0f5b7d2c8214
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-03-31 21:14:43 +0800

    banner-height is reduced

--- resources/assets/js/sections/banner.js
+++ resources/assets/js/sections/banner.js
@@ -6,7 +6,7 @@ import { DefaultPanel } from "../components/Panel";
 export class Banner extends React.Component {
     render() {
         return (
-            <Row className="full-height flex-center">
+            <Row className="full-height banner flex-center">
                 <Col md={12}>
                     <img src="/images/coming_soon.jpg" alt="coming-soon" className="img-responsive" />
                 </Col>
