ce23b195949de5e3faadd51f3dfc8061e0778f2c
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-04-09 19:49:35 +0800

    change the title website

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>GTD Laravel - React</title>
+        <title>GTD</title>
 
 		{{-- favicon --}}
 		<link rel="shortcut icon" href="/images/favicon.png" type="image/x-icon" />
