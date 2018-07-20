e6e2daa7ce3ef69a61be3bcef304acc1fdbf6e56
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-07-20 11:24:15 +0700

    Reformat

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,28 +1,28 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>GTD</title>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>GTD</title>
 
-		{{-- favicon --}}
-		<link rel="shortcut icon" href="/images/favicon.png" type="image/x-icon" />
+  {{-- favicon --}}
+  <link rel="shortcut icon" href="/images/favicon.png" type="image/x-icon"/>
 
-        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 
-        <!-- Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        
-    </head>
-    <body>
-        
-        @yield('content')
 
-		<script src="{{ asset('js/main.js') }}"></script>
+</head>
+<body>
 
-		@yield('script')
-    </body>
+@yield('content')
+
+<script src="{{ asset('js/main.js') }}"></script>
+
+@yield('script')
+</body>
 </html>
