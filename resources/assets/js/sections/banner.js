d7292ee67095d52ac2689d4743d19937b45dcff7
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-07-25 22:56:23 +0800

    Remove the full height class

--- resources/assets/js/sections/banner.js
+++ resources/assets/js/sections/banner.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
-import { DefaultPanel } from "../components/Panel";
 
-
-export class Banner extends React.Component {
-    render() {
-        return (
-            <Row className="full-height banner flex-center">
-                <Col md={12} className="banner-container">
-                  <a href="/exodia">
-                    <img src="/images/coming_soon.jpg" alt="coming-soon" className="banner-responsive dhea" />
-                  </a>
-                </Col>
-            </Row>
-        )
-    }
-}
+export const Banner = () => {
+  return (
+    <Row className="banner flex-center">
+      <Col md={12} className="banner-container">
+        <a href="/exodia">
+          <img src="/images/coming_soon.jpg" alt="coming-soon" className="banner-responsive dhea"/>
+        </a>
+      </Col>
+    </Row>
+  )
+};
