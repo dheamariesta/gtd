aec9e6be87ab0ff98bd1ae5132ac08e21b3733c0
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-08-03 02:16:37 +0800

    Display navbar-right only when on home page

--- resources/assets/js/components/Topbar.js
+++ resources/assets/js/components/Topbar.js
@@ -1,61 +1,64 @@
 // Components
 import React from "react";
 import { Navbar, Nav, NavItem, Image } from "react-bootstrap";
-import Home from "../pages/Home";
 
 
 export class HomeTopbar extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	static scrollToSection() {
-		$("a").on('click', function (event) {
-			// Make sure this.hash has a value before overriding default behavior
-			if (this.hash !== "") {
-				// Prevent default anchor click behavior
-				event.preventDefault();
+  static scrollToSection() {
+    $("a").on('click', function (event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
 
-				// Store hash
-				let hash = this.hash;
+        // Store hash
+        let hash = this.hash;
 
-				$('html, body').animate({
-					scrollTop: $(hash).offset().top
-				}, 1500, function () {
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1500, function () {
 
-					// Add hash (#) to URL when done scrolling (default click behavior)
-					window.location.hash = hash;
-				});
-			} // End if
-		});
-	}
-	componentDidMount() {
-		HomeTopbar.scrollToSection();
-	}
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  }
 
-	render() {
-		const { isDesktop } = this.props;
-		return (
-			<Navbar fixedTop={isDesktop} id="navbar-scrollspy">
-				<Navbar.Header>
-					<Navbar.Brand>
-						<a href="/">
-							<img src="/images/logo.png" alt="logo" className="img-responsive hidden-xs" />
-						</a>
-					</Navbar.Brand>
-					<Navbar.Toggle />
-				</Navbar.Header>
-				<Navbar.Collapse>
-					<Nav pullRight>
-						<NavItem eventKey={1} href="#about">About</NavItem>
-						<NavItem eventKey={2} href="#history">History</NavItem>
-						<NavItem eventKey={3} href="#anthem">Anthem</NavItem>
-            <NavItem eventKey={4} href="#faq">FAQ</NavItem>
-					</Nav>
-				</Navbar.Collapse>
-			</Navbar>
-		);
-	}
+  componentDidMount() {
+    HomeTopbar.scrollToSection();
+  }
+
+  render() {
+    const { isDesktop } = this.props;
+    return (
+      <Navbar fixedTop={isDesktop} id="navbar-scrollspy">
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">
+              <img src="/images/logo.png" alt="logo" className="img-responsive hidden-xs"/>
+            </a>
+          </Navbar.Brand>
+          <Navbar.Toggle/>
+        </Navbar.Header>
+        {window.location.pathname === '/' ? (
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#about">About</NavItem>
+              <NavItem eventKey={2} href="#history">History</NavItem>
+              <NavItem eventKey={3} href="#anthem">Anthem</NavItem>
+              <NavItem eventKey={4} href="#faq">FAQ</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        ) : ("")
+        }
+      </Navbar>
+    );
+  }
 }
 
 export const ExodiaTopBar = () => {
