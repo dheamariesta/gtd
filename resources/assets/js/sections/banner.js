06ddbe566d48688a4530a9d9b2e12f1451e629e3
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-08-06 02:35:32 +0800

    Add padding-top for desktop for banner

--- resources/assets/js/sections/banner.js
+++ resources/assets/js/sections/banner.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Image } from "react-bootstrap";
+import { screenMdMin } from "../helpers/breakpoint";
+
+import styled from "styled-components";
+
+
+const StyledContainer = styled.div`
+  padding-right: 0;  
+  padding-left: 0;
+  @media(min-width: ${screenMdMin}){
+    padding-top: 50px; 
+  }
+  img {
+    width: 100vw;
+  }
+`;
 
 export const Banner = () => {
   return (
-    <Row className="banner flex-center">
-      <Col md={12} className="banner-container">
+    <Row className="flex-center">
+      <StyledContainer>
         <a href="/exodia">
-          <img src="/images/coming_soon.jpg" alt="coming-soon" className="banner-responsive dhea"/>
+          <Image src="/images/coming_soon.jpg" alt="banner" responsive />
         </a>
-      </Col>
+      </StyledContainer>
     </Row>
   )
 };
