3f41891d4c4e88987746aba97ef1a94a3b47ecb0
unknown <hanstananda@gmail.com>
2018-08-08 11:41:30 +0800

    update js based on sheet

--- resources/assets/js/sections/partials/Day2.js
+++ resources/assets/js/sections/partials/Day2.js
@@ -7,7 +7,9 @@ export class Day2_table extends React.Component {
         super(props);
         this.componentDidMount = this.componentDidMount.bind(this);
         this.state = {
-            game_balance_beach: ''
+            game_balance_beach: '',
+            beach_min_bid: '',
+            beach_max_bid: ''
         };
     }
 
@@ -21,8 +23,12 @@ export class Day2_table extends React.Component {
         }).then(response => {
             //console.log(response.data);
             const balance_beach = response.data.game_balance_beach;
+            const max_bid = response.data.beach_max_bid;
+            const min_bid = response.data.beach_min_bid;
             this.setState({
-                game_balance_beach: balance_beach
+                game_balance_beach: balance_beach,
+                beach_min_bid: min_bid,
+                beach_max_bid: max_bid
             });
             console.log("Day2 fetched!");
 
@@ -34,16 +40,22 @@ export class Day2_table extends React.Component {
     
     render(){
         const listScores = this.state.game_balance_beach;
+        const minBid = this.state.beach_min_bid;
+        const maxBid = this.state.beach_max_bid
         return(
 			<div className="Day2_table">
                 <Row className="default-bg">
                 <div className="container">
-                <h3>Score</h3>
+                <h3>Score:</h3>
                 <h3>{listScores[0]}</h3>
-                <h3>Minimum Bet</h3>
-                <h3>{listScores[0]}</h3>
-                <h3>Maximum Bet</h3>
-                <h3>{listScores[0]}</h3>
+                <Col md={6}>
+                <h3>Minimum Bet:</h3>
+                <h3>{minBid[0]}</h3>
+                </Col>
+                <Col md={6}>
+                <h3>Maximum Bet:</h3>
+                <h3>{maxBid[0]}</h3>
+                </Col>
                 </div>
                 </Row>
             </div>
