7bc3620d21ea202531cb66c0c13318f722d69a7e
unknown <hanstananda@gmail.com>
2018-08-08 11:12:20 +0800

    update js based on sheet

--- resources/assets/js/pages/score.js
+++ resources/assets/js/pages/score.js
@@ -139,7 +139,6 @@ export class Score extends React.Component {
                                 <h2 className="section-title title">Beach Games</h2>
                             </Col>
                             <Col md={12}>
-                                <Day2_table component OG_NAME={this.state.OG_NAME}/>
                             </Col>
                         </Row>
                     </div>
@@ -150,7 +149,6 @@ export class Score extends React.Component {
                                 <h2 className="section-title title">Field Games</h2>
                             </Col>
                             <Col md={12}>
-                                <Day3_table component OG_NAME={this.state.OG_NAME}/>
                             </Col>
                         </Row>
                     </div>
